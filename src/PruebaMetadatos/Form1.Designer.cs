606a1e5ab16ec9c4ca931048b132c8b5c0dceee5
Cotel <miguelcoletomunoz@hotmail.com>
2015-06-22 17:11:18 +0200

    Improvements Added
    - Now it can delete metadata strings
    - Minor improvements for the GUI

--- src/PruebaMetadatos/Form1.Designer.cs
+++ src/PruebaMetadatos/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,18 +83,29 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(388, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 53);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete Metadata";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 641);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Metadata Controller";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -106,6 +118,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- src/PruebaMetadatos/Form1.cs
+++ src/PruebaMetadatos/Form1.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,30 +16,40 @@ namespace PruebaMetadatos
     public partial class Form1 : Form
     {
         OpenFileDialog data = new OpenFileDialog();
+        Boolean open = false;
 
         public Form1()
         {
             InitializeComponent();
+            listBox2.Items.Add("Load an Image File to begin...");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             data.Title = "Open First Image File";
             data.InitialDirectory = "@C:\\";
-            data.Filter = "JPF files (*.jpg)|*.jpg|All files (*.*)|(*.*)";
+            data.Filter = "Image files (*.jpg; *.gif; *png)|*.jpg;*.jpeg;*.png;*.gif|All files (*.*)|*.*";
             data.FilterIndex = 1;
             data.RestoreDirectory = true;
 
             if (data.ShowDialog() == DialogResult.OK)
             {
-                pictureBox1.Load(data.FileName);
-            }
+                try
+                {
+                    pictureBox1.Load(data.FileName);
+                    open = true;
+                }
+                catch
+                {
+                    MessageBox.Show("That file is not an image!", "File format exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
+            }
         }
 
         public void meta()
         {
-            Bitmap image = new Bitmap(data.FileName);
+            Bitmap image = new Bitmap(@data.FileName);
             PropertyItem[] propItems = image.PropertyItems;
             ASCIIEncoding encodings = new ASCIIEncoding();
 
@@ -120,65 +132,13 @@ namespace PruebaMetadatos
                         string serial = encodings.GetString(item.Value);
                         listBox2.Items.Add("The serial number is " + serial + ".");
                         break;
-
-
                 }
             }
-            
-            //try
-            //{
-            //    string make = encodings.GetString(propItems[1].Value);
-            //    listBox2.Items.Add("The equipment make is " + make + ".");
-            //}
-            //catch
-            //{
-            //    listBox2.Items.Add("no Meta Data Found");
-            //}
-
-            //try
-            //{
-            //    string model = encodings.GetString(propItems[2].Value);
-            //    listBox2.Items.Add("The model is " + model + ".");
-            //}
-            //catch
-            //{
-            //    listBox2.Items.Add("no Model Found");
-            //}
-
-            //try
-            //{
-            //    string soft = encodings.GetString(propItems[4].Value);
-            //    listBox2.Items.Add("The Software is " + soft + ".");
-            //}
-            //catch
-            //{
-            //    listBox2.Items.Add("no Software Found");
-            //}
-
-            //try
-            //{
-            //    string DT = encodings.GetString(propItems[15].Value);
-            //    listBox2.Items.Add("The Date & Time is " + DT + ".");
-            //}
-            //catch
-            //{
-            //    listBox2.Items.Add("no Date & Time Found");
-            //}
-
-            //try
-            //{
-            //    string lastDT = encodings.GetString(propItems[5].Value);
-            //    listBox2.Items.Add("The Last Modification Date is " + lastDT + ".");
-            //} 
-            //catch
-            //{
-            //    listBox2.Items.Add("no Last Modification Date Found");
-            //}
+            image.Dispose();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -186,6 +146,85 @@ namespace PruebaMetadatos
             listBox2.Items.Clear();
             listBox1.Items.Clear();
             meta();
+        }
+
+        //public Stream PatchAwayExif(Stream inStream, Stream outStream)
+        //{
+        //    byte[] jpegHeader = new byte[2];
+        //    jpegHeader[0] = (byte)inStream.ReadByte();
+        //    jpegHeader[1] = (byte)inStream.ReadByte();
+        //    if (jpegHeader[0] == 0xff && jpegHeader[1] == 0xd8)
+        //    {
+        //        SkipAppHeaderSection(inStream);
+        //    }
+        //    outStream.WriteByte(0xff);
+        //    outStream.WriteByte(0xd8);
+
+        //    int readCount;
+        //    byte[] readBuffer = new byte[4096];
+        //    while ((readCount = inStream.Read(readBuffer, 0, readBuffer.Length)) > 0){
+        //        outStream.Write(readBuffer, 0, readCount);
+        //    }
+        //    return outStream;
+        //}
+
+        //private void SkipAppHeaderSection(Stream inStream)
+        //{
+        //    byte[] header = new byte[2];
+        //    header[0] = (byte)inStream.ReadByte();
+        //    header[1] = (byte)inStream.ReadByte();
+
+        //    while (header[0] == 0xff && (header[1] >= 0xe0 && header[1] <= 0xef))
+        //    {
+        //        int exifLength = inStream.ReadByte();
+        //        exifLength = exifLength << 8;
+        //        exifLength |= inStream.ReadByte();
+
+        //        for (int i = 0; i < exifLength - 2; i++)
+        //        {
+        //            inStream.ReadByte();
+        //        }
+        //        header[0] = (byte)inStream.ReadByte();
+        //        header[1] = (byte)inStream.ReadByte();
+        //    }
+        //    inStream.Position -= 2;
+
+        //}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (open)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Title = "Save an Image File";
+                saveFileDialog1.Filter = "Image files (*.jpg; *.gif; *png)|*.jpg;*.jpeg;*.png;*.gif|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.FileName = data.FileName;
+
+                Bitmap image = new Bitmap(data.FileName);
+                PropertyItem[] propItems = image.PropertyItems;
+
+                foreach (PropertyItem item in propItems)
+                {
+                    if (item.Type == 2)
+                    {
+                        image.RemovePropertyItem(item.Id);
+                    }
+                }
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != "")
+                {
+                    if (saveFileDialog1.FileName == data.FileName)
+                    {
+                        MessageBox.Show("For security reasons I can't overwrite!", "Overwrite not allowed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        image.Save(saveFileDialog1.FileName);
+                    }
+                }
+                image.Dispose();
+            }
         }
     }
 }
